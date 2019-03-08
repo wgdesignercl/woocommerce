6ac0a3dfc90f8ce2f398d1ae1445a8b5b7c0ca29
wladimir <wladimir.nova.parra@gmail.com>
2019-03-08 19:38:51 -0300

    Create ocultar-mensajes-notificaciones-wordpress.php

---
+++ ocultar-mensajes-notificaciones-wordpress.php
@@ -0,0 +1,21 @@
+<?php
+//* Eliminar notificaciones de plugins, actualizaciones, errores etc..
+function hide_notices_dashboard() {
+    global $wp_filter;
+ 
+    if (is_network_admin() and isset($wp_filter["network_admin_notices"])) {
+        unset($wp_filter['network_admin_notices']);
+    } elseif(is_user_admin() and isset($wp_filter["user_admin_notices"])) {
+        unset($wp_filter['user_admin_notices']);
+    } else {
+        if(isset($wp_filter["admin_notices"])) {
+            unset($wp_filter['admin_notices']);
+        }
+    }
+ 
+    if (isset($wp_filter["all_admin_notices"])) {
+        unset($wp_filter['all_admin_notices']);
+    }
+}
+add_action( 'admin_init', 'hide_notices_dashboard' );?>
+ 
