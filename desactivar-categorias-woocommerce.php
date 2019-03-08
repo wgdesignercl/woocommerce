1b578a29f7d7d62adfe425e489ae6264e25ac259
wladimir <wladimir.nova.parra@gmail.com>
2019-03-08 18:40:14 -0300

    Update desactivar-categorias-woocommerce.php

--- desactivar-categorias-woocommerce.php
+++ desactivar-categorias-woocommerce.php
@@ -2,6 +2,6 @@
 //* NO incluyas la etiqueta de apertura
 add_filter( 'woocommerce_product_categories_widget_args', 'woo_product_cat_widget_args' );
 function woo_product_cat_widget_args( $cat_args ) {
-	$cat_args['exclude'] = array('18'); //* Incluye el ID de la categoría que deseas excluir
+	$cat_args['exclude'] = array('18','19'); //* Incluye el ID de la categoría que deseas excluir
 	return $cat_args;
 }
